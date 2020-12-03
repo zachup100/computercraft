a7c31a091d27b769ff27a2d5c943c215608e35d1
zachup100 <zachup100@users.noreply.github.com>
2020-12-03 09:43:13 -0500

    reactorv1

--- reactor_server.lua
+++ reactor_server.lua
@@ -1,18 +1,2 @@
-function wait(n)
-    n = tonumber(n) or 0.3
-    local start = os.clock()
-    repeat until (os.clock()-start) >= n
-    return true
-end
-
-
-
-
-
-
-function start_server()
-    wait(1)
-    print("Hello!")
-end
-
-coroutine.wrap(start_server)()
+local reactor = peripheral.find("BigReactor-Reactor")
+print(reactor~=nil)
