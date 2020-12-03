8ff4c0d319c10d5af44a3ac6c80717a5917c2ffe
zachup100 <zachup100@users.noreply.github.com>
2020-12-03 09:41:12 -0500

    test

--- reactor_server.lua
+++ reactor_server.lua
@@ -1,5 +1,17 @@
+function wait(n)
+    n = tonumber(n) or 0.3
+    local start = os.clock()
+    repeat until (os.clock()-start) >= n
+    return true
+end
+
+
+
+
+
+
 function start_server()
-    os.sleep(3)
+    wait(1)
     print("Hello!")
 end
 
