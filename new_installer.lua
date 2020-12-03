1451d6035707abb01cdefedcfbad45e5782cb5f5
zachup100 <zachup100@users.noreply.github.com>
2020-12-03 10:00:48 -0500

    ddd

--- new_installer.lua
+++ new_installer.lua
@@ -2,4 +2,5 @@ local Index = {...}
 local Author = Index[1]
 local Repository = Index[2]
 local _Program = shell.getRunningProgram()
-if not Author or not Repository then print(string.format("%s", _Program)) end
+if not Author or not Repository then print(string.format("%s <Author> <Repository>", _Program)) bExit = true end
+error("Hi")
