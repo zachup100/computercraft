cce26ff0e0dc4952f646ad0f0fb579b1046c5647
zachup100 <zachup100@users.noreply.github.com>
2020-12-04 05:34:47 -0500

    changes made

--- phone-weather.lua
+++ phone-weather.lua
@@ -2,6 +2,24 @@ local Height, Width = term.getSize()
 if Height ~= 26 or Width ~= 20 then error("This program can only run on a pocket computer.") end
 term.clear()
 
+local Buttons = {}
+
+function AddButton(Title,X,Y,MX,MY,func)
+  if type(func) ~= "function" then return end
+  Title = tostring(Title)
+  if type(Buttons[Title]) ~= "nil" then return end
+  X,Y,MX,MY = (tonumber(X) or 1),(tonumber(Y) or 1),(tonumber(MX) or 1),(tonumber(MY) or 1)
+  local Data = {X=X,Y=Y,MX=MX,MY=MY,func=func}
+  Buttons[Title] = Data
+end
+
+function WithinButton(Title, X, Y)
+  Title = tostring(Title)
+  if type(Buttons[Title]) == "nil" then return false end
+  local Data = Buttons[Title]
+  if X >= Data.X and Y >= Data.Y and X <= Data.MX and Y <= Data.MY then return true end
+  return false
+end
 
 --// Outline
 term.setBackgroundColor(colors.blue)
@@ -16,4 +34,15 @@ term.setCursorPos(2,2)
 for i=2,24,1 do
   term.setCursorPos(2,i)
   term.write((" "):rep(19))
+end
+
+AddButton(1,1,1,1, function() rednet.broadcast("stop_rain") end)
+
+while true do
+  local Event, PosX, PosY = os.pullEvent("mouse_click")
+  for Button, Data in pairs(Buttons) do
+    if WithinButton(Button, PosX, PosY) then
+      Data.func()
+    end
+  end
 end
