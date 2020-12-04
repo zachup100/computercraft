5c4ebecc250310a7a3e114cd428ea8972db21c70
zachup100 <zachup100@users.noreply.github.com>
2020-12-04 00:15:14 -0500

    tricky

--- farming.lua
+++ farming.lua
@@ -1,5 +1,25 @@
+local StartupParameters = {...}
+local FarmSize = {X=(tonumber(StartupParameters[1]) or 2),Z=(tonumber(StartupParameters[1]) or 2)}
+
+
+
+
+
+
+
+
+
+
+
 local Parameters = {...}
-local SizeX, SizeZ = tonumber(Parameters[1]) or 4, tonumber(Parameters[1]) or 4
+local SizeX, SizeZ = tonumber(Parameters[1]) or 4, tonumber(Parameters[2]) or 4
 local Heading = 0
+
 --// North = 0, East = 1, South = 2, West = 3
-print("Arguments:",SizeX, SizeZ)
+
+term.clear()
+term.setCursorPos(1,1)
+
+function CanMove()
+
+end
