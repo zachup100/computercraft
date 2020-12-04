4bb62b550177a96bc1684ef9ca8c40369c868d44
zachup100 <zachup100@users.noreply.github.com>
2020-12-04 04:56:49 -0500

    test

---
+++ phone-weather.lua
@@ -0,0 +1,7 @@
+local Height, Width = term.getSize()
+if Height ~= 26 or Width ~= 20 then error("This program can only run on a pocket computer.") end
+term.clear()
+
+--// Draw
+term.setBackgroundColor(colors.lightBlue)
+term.write("     ")
