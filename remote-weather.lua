e6b04c535d37a263528fc2c44f75c062855056ae
zachup100 <zachup100@users.noreply.github.com>
2020-12-04 03:40:46 -0500

    wtf

--- remote-weather.lua
+++ remote-weather.lua
@@ -3,6 +3,7 @@ function GetModem()
   for _, Port in pairs(Connections) do
     local Methods = peripheral.getMethods(Port)
     for _, func in pairs(Methods) do
+      print(func)
       if func == "isWireless" and Methods.isWireless() == true then
         return tostring(Port)
       end
