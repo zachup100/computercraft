6fb9311950cf806c4f6a7bbefe59d14ede40e2b8
zachup100 <zachup100@users.noreply.github.com>
2020-12-03 23:19:03 -0500

    check1

--- farming.lua
+++ farming.lua
@@ -2,4 +2,4 @@ local Parameters = {...}
 local SizeX, SizeZ = tonumber(Parameters[1]) or 4, tonumber(Parameters[1]) or 4
 local Heading = 0
 --// North = 0, East = 1, South = 2, West = 3
-print(SizeX, SizeZ)
+print("Arguments:",SizeX, SizeZ)
