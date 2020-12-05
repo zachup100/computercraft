d20fd322a3cd6cb5179eb1d64ccc39a932c36092
zachup100 <zachup100@users.noreply.github.com>
2020-12-05 13:04:04 -0500

    API Test

--- api/buttons.lua
+++ api/buttons.lua
@@ -1,10 +1,33 @@
-Buttons = {}
+local Buttons = {}
+--// Registered[Title] = Data
+new(Title, X, Y, SizeX, SizeY)
 
-Buttons:new(Title, X, Y, SizeX, SizeY)
+  
+
   local btn = {}
+  btn.Title = ""
+  btn.Enabled = true
+  btn.Position = {X=1,Y=1}
+  btn.Size = {X=1,Y=SizeY}
 
-  function btn:getTitle()
+  function btn:getName() return tostring(btn.Title) end
+  function btn:getSize() return btn.Size.X, btn.Size.Y end
+  function btn:getPosition() return btn.Position.X, btn.Position.Y end
 
+  function btn:setSize(X, Y)
+    if type(X) ~= "number" and type(Y) ~= "number" then
+      btn.Size = {X=X,Y=Y}
+      return true
+    end
+    return false
+  end
+
+  function btn:setPosition(X, Y)
+    if type(X) ~= "number" and type(Y) ~= "number" then
+      btn.Position = {X=X,Y=Y}
+      return true
+    end
+    return false
   end
 
   return btn
