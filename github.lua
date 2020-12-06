7b951aa03d250dd663c2f77bdf2532ff6aee599a
zachup100 <zachup100@users.noreply.github.com>
2020-12-06 06:10:00 -0500

    Parse JSONs

--- github.lua
+++ github.lua
@@ -10,19 +10,22 @@ if type(Author) ~= "string" or type(Repository) ~= "string" then
   return
 end
 
-shell.run("pastebin", "get", "4nRg9CHU", "rom/apis/json.lua")
-if not fs.exists("rom/apis/json.lua") then
+if not fs.exists("apis") or fs.exists("apis") and not fs.isDir("apis") then shell.run("mkdir", "apis") end
+shell.run("pastebin", "get", "4nRg9CHU", "apis/json.lua")
+
+if not fs.exists("apis/json.lua") then
   print(string.format("%s: Failed to download json library", shell.getRunningProgram()))
   return
 end
 
 local GIT_INFO_URL = string.format("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", Author, Repository, Branch)
 local CONTENT_URL = string.format("https://raw.githubusercontent.com/%s/%s/%s/", Author, Repository, Branch)
+local JSON = os.loadAPI("apis/json.lua")
 
 function GetRepositoryInfo()
   local Success = http.get(GIT_INFO_URL)
   if Success then
-    local Contents = Success.readAll()
+    local Contents = JSON.parseValue(Success.readAll())
     Success.close()
     return true, Contents
   end
@@ -32,7 +35,7 @@ end
 function GetFileContents(path)
   local Success = http.get(string.format(CONTENT_URL..path))
   if Success then
-      local Contents = Success.readAll()
+      local Contents = JSON.parseValue(Success.readAll())
       Success.close()
       return true, Contents
   end
