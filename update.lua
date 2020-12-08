31029f7f04b36ed78fa0dd8324925092146eb5fa
zachup100 <zachup100@users.noreply.github.com>
2020-12-08 11:36:04 -0500

    Added option to update to specific branch

--- update.lua
+++ update.lua
@@ -1,9 +1,12 @@
+local Parameters = {...}
+local Branch = string.lower((type(Parameters[1])=="string" and Parameters[3]) or "master")
+
 local function GetInstallPath()
   local Program = shell.getRunningProgram()
   local Name = fs.getName(Program)
   return "/"..Program:sub(1, #Program-#Name)
 end
 
-shell.run("pastebin", "run", "Eky25aPJ", "zachup100", "computercraft", "master", GetInstallPath())
+shell.run("pastebin", "run", "Eky25aPJ", "zachup100", "computercraft", Branch, GetInstallPath())
 sleep(3)
 os.reboot()
