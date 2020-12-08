3f5b4d2826d15b10acda06c4fd1fb4314c79cb35
zachup100 <zachup100@users.noreply.github.com>
2020-12-08 10:58:43 -0500

    Directory obtaining test

--- update.lua
+++ update.lua
@@ -1,3 +1,10 @@
+local function GetInstallPath()
+  local Program = shell.getRunningProgram()
+  local Name = fs.getName(Program)
+  return Program:sub(1, #Program-#Name)
+end
+
+print(GetInstallPath())
 shell.run("pastebin", "run", "Eky25aPJ", "zachup100", "computercraft", "master", "/")
 sleep(3)
 os.reboot()
