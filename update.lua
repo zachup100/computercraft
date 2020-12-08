b2149e497f237652247d9009e4aa108543f194ce
zachup100 <zachup100@users.noreply.github.com>
2020-12-08 11:05:09 -0500

    Fixes

--- update.lua
+++ update.lua
@@ -1,7 +1,7 @@
 local function GetInstallPath()
   local Program = shell.getRunningProgram()
   local Name = fs.getName(Program)
-  return Program:sub(1, #Program-#Name)
+  return "/"..Program:sub(1, #Program-#Name)
 end
 
 print(GetInstallPath())
