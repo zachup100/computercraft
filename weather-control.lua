35cad59b911ba02196b784bdd2819119a8547d10
zachup100 <zachup100@users.noreply.github.com>
2020-12-08 13:15:10 -0500

    revamp testing

---
+++ weather-control.lua
@@ -0,0 +1,35 @@
+local function GetInstallPath()
+  local Program = shell.getRunningProgram()
+  local Name = fs.getName(Program)
+  return "/"..Program:sub(1, #Program-#Name)
+end
+
+local OUTLINE_COLOR = colors.blue
+local BACKGROUND_COLOR = colors.white
+
+local ButtonsAPI = GetInstallPath().."/api/buttons.lua"
+if not pocket then error("This program can only run on pocket computers.") end
+if not fs.exists(ButtonsAPI) or not os.loadAPI(ButtonsAPI) then error("Failed to load Buttons API") end
+rednet.open("back")
+term.clear()
+
+function DrawWindow()
+  local Width, Height = term.getSize()
+  --// Outline
+  term.setBackgroundColor((type(OUTLINE_COLOR)=="number" and OUTLINE_COLOR) or colors.black)
+  for i=1,Height,1 do
+    term.setCursorPos(1,i)
+    term.write((" "):rep(Width))
+  end
+  --// Background
+  term.setBackgroundColor((type(BACKGROUND_COLOR)=="number" and BACKGROUND_COLOR) or colors.black)
+  for i=2,Height-2,1 do
+    term.setCursorPos(2,i)
+    term.write((" "):rep(Width-2))
+  end
+end
+
+while true do
+  DrawWindow()
+  sleep(3)
+end
